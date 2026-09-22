7d746c306b81c61ef8d89a707779caba13a2f40d
mojtaba0jafari <115713684+Mojtaba0jafari@users.noreply.github.com>
2026-09-22 15:38:14 +0330

    Add Todo domain model

---
+++ Domain/Todo.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoList.Api.Domain
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public bool Iscompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? Deadline { get; set; }
+    }
+}
